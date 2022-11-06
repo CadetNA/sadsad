8d599becfa11cf6b438e1200741b8b45f3deef8e
CadetNA <89815718+CadetNA@users.noreply.github.com>
2022-11-05 23:51:12 -0400

    Update rename.js

--- src/bot/commands/rename.js
+++ src/bot/commands/rename.js
@@ -11,8 +11,8 @@ module.exports = {
             if (!message.channel.name.includes("ticket-")) {
                 message.channel.send({
                     embed: {
-                        title: `**❌ | Error**`,
-                        description: `This is not a ticket channel`,
+                        title: `**ERROR**`,
+                        description: `This channel is not for tickets.`,
                         color: 0xFF0000
                     }
                 }).then(async function(msg) {
@@ -25,8 +25,8 @@ module.exports = {
             if (!message.member.hasPermission('MANAGE_CHANNELS')) {
                 message.channel.send({
                     embed: {
-                        title: `**❌ | Error**`,
-                        description: `you need same permissions to use this command`,
+                        title: `**ERROR**`,
+                        description: `You do not have the correct permissions.`,
                         color: 0xFF0000
                     }
                 }).then(async function(msg) {
@@ -39,8 +39,8 @@ module.exports = {
             if (!renameMessage) {
                 message.channel.send({
                     embed: {
-                        title: `**❌ | Error**`,
-                        description: `you need same permissions to use this command`,
+                        title: `**ERROR**`,
+                        description: `You do not have the correct permissions.`,
                         color: 0xFF0000
                     }
                 }).then(async function(msg) {
@@ -63,8 +63,8 @@ module.exports = {
                 .addComponent(btn2);
             message.channel.send({
                 embed: {
-                    title: `**⚠️ | Confirmation**`,
-                    description: `Are you sure about rename this ticket?`,
+                    title: `*WARNING**`,
+                    description: `Are you sure you want to rename this ticket?`,
                     color: 0xFFF200
                 },
                 component: row

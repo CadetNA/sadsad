5de4d6f479538d46a3ac0bda17dad05fcd281722
CadetNA <89815718+CadetNA@users.noreply.github.com>
2022-11-05 23:52:04 -0400

    Update help.js

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -7,16 +7,18 @@ module.exports = {
         try {
           message.channel.send({
             embed: {
-              title: 'Bot Commands',
+              title:
+                '!help',
+              color: 
+                000053,
               description: `
-                            help
-                            setup
-                            open
-                            add
-                            remove
-                            rename
-                            prefix
-                            ping
+                            help - Opens up the help Menu. Usage: !help
+                            setup - Setup the bot. Usage: !setup <Ticket Channel> <Ticket Workers Role> <Ticket Title>
+                            open - Opens a new ticket. Usage: !open
+                            add - Adds a member to the ticket. Usage: <!add @username>
+                            remove - Removes a member from the ticket. Usage: !remove <@username>
+                            rename - Renames the ticket. Usage: !rename <name>
+                            ping - Requests the bot response time. Usage: !ping
                            `
             }
           })
